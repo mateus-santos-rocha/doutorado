42081411ec94efc902ee0f4f67eed4593ff637a0
mateus-santos-rocha <mateusdonca97@gmail.com>
2025-05-07 16:55:45 -0300

    etl for bronze tables with station data

---
+++ etl.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import duckdb
+
+conn = duckdb.connect("bronze_db")
+
+estacoes_bronze_tables_dict = {
+    'dim_estacoes':'bronze/dim_estacoes.csv',
+    'fato_estacoes_precipitacao':'bronze/fato_estacoes_precipitacao.csv',
+    'fato_estacoes_temperatura_media':'bronze/fato_estacoes_temperatura_media.csv',
+    'fato_estacoes_temperatura_minima':'bronze/fato_estacoes_temperatura_minima.csv',
+    'fato_estacoes_umidade_relativa_maxima':'bronze/fato_estacoes_umidade_relativa_maxima.csv',
+    'fato_estacoes_umidade_relativa_media':'bronze/fato_estacoes_umidade_relativa_media.csv',
+    'fato_estacoes_umidade_relativa_minima':'bronze/fato_estacoes_umidade_relativa_minima.csv',
+    'fato_estacoes_velocidade_vento_2m_maxima':'bronze/fato_estacoes_velocidade_vento_2m_maxima.csv',
+    'fato_estacoes_velocidade_vento_2m_media':'bronze/fato_estacoes_velocidade_vento_2m_media.csv',
+    'fato_estacoes_velocidade_vento_10m_media':'bronze/fato_estacoes_velocidade_vento_10m_media.csv',
+}
+
+for table_name,table_path in estacoes_bronze_tables_dict.items():
+    conn.execute(f"""
+        CREATE TABLE {table_name} AS
+            SELECT
+                *
+            FROM read_csv('{table_path}')
+""")
+    
+conn.execute("select * from bronze_db.fato_estacoes_precipitacao").fetch_df()
