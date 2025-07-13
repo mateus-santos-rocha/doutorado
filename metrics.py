d8712ea673e1bd4ec1b8d4979a9352b60744f559
mateus-santos-rocha <mateusdonca97@gmail.com>
2025-07-12 22:18:48 -0300

    metrics file

---
+++ metrics.py
@@ -0,0 +1,57 @@
+from sklearn.metrics import r2_score,root_mean_squared_error,mean_absolute_error
+import numpy as np
+
+def R2_determinacao(y_pred,y_true):
+    return r2_score(y_pred,y_true)
+
+def RMSE(y_pred,y_true):
+    return root_mean_squared_error(y_pred,y_true)
+
+def MAE(y_pred,y_true):
+    return mean_absolute_error(y_pred,y_true)
+
+def PSC(y_true,y_pred):
+    y_true,y_pred = np.array(y_true),np.array(y_pred)
+    
+    indices_sem_chuva = y_true == 0
+    n_casos_sem_chuva = np.sum(indices_sem_chuva)
+
+    predicoes_corretas = np.sum(y_pred[indices_sem_chuva] == 0)
+    psc = (predicoes_corretas / n_casos_sem_chuva) * 100 if n_casos_sem_chuva > 0 else 0.0
+    return psc
+
+def PSC_A(y_true,y_pred,erro):
+    y_true,y_pred = np.array(y_true),np.array(y_pred)
+    
+    indices_sem_chuva = y_true == 0
+    n_casos_sem_chuva = np.sum(indices_sem_chuva)
+
+    predicoes_corretas = np.sum(y_pred[indices_sem_chuva] <= erro)
+    psc_a = (predicoes_corretas / n_casos_sem_chuva) * 100 if n_casos_sem_chuva > 0 else 0.0
+    return psc_a
+
+def PCC_A(y_true, y_pred, erro):
+    y_true,y_pred = np.array(y_true),np.array(y_pred)
+    
+    indices_com_chuva = y_true > 0
+    total_chuva = np.sum(indices_com_chuva)
+    
+    predicoes_corretas = np.sum(
+        np.abs(y_true[indices_com_chuva] - y_pred[indices_com_chuva]) <= erro
+    )
+    pcc_a = (predicoes_corretas / total_chuva) * 100 if total_chuva > 0 else 0.0
+    
+    return pcc_a
+
+def PMC_A(y_true, y_pred, erro, chuva_minima):
+    y_true,y_pred = np.array(y_true),np.array(y_pred)
+
+    indices_chuva_pesada = y_true > chuva_minima
+    total_chuva_pesada = np.sum(indices_chuva_pesada)
+    
+    previsoes_corretas = np.sum(
+        np.abs(y_true[indices_chuva_pesada] - y_pred[indices_chuva_pesada]) <= erro
+    )
+    pmc_a = (previsoes_corretas / total_chuva_pesada) * 100 if total_chuva_pesada > 0 else 0.0
+    
+    return pmc_a
---
+++ modelling_utils.py
@@ -0,0 +1,33 @@
+def get_abt_estacoes_vizinhas(modelling_conn,abt_estacoes_vizinhas_table_name = 'abt_estacoes_vizinhas',create_dt_columns=True):
+    abt_estacoes_vizinhas = modelling_conn.execute(f"""SELECT * FROM {abt_estacoes_vizinhas_table_name}""").fetchdf()
+    if create_dt_columns:
+        abt_estacoes_vizinhas['dt_medicao_mes'] = abt_estacoes_vizinhas['dt_medicao'].dt.month
+        abt_estacoes_vizinhas['dt_medicao_ano'] = abt_estacoes_vizinhas['dt_medicao'].dt.year
+    return abt_estacoes_vizinhas
+
+def split_com_sem_vizinha(abt_estacoes_vizinhas,threshold_prioridade):
+    abt_com_vizinha = abt_estacoes_vizinhas \
+        .loc[abt_estacoes_vizinhas['vl_prioridade_vizinha'].fillna(0) >= threshold_prioridade] \
+        .sort_values(by=['id_estacao','dt_medicao'])
+
+    abt_sem_vizinha = abt_estacoes_vizinhas \
+        .loc[abt_estacoes_vizinhas['vl_prioridade_vizinha'].fillna(0) < threshold_prioridade] \
+        .sort_values(by=['id_estacao','dt_medicao'])
+
+    return abt_com_vizinha,abt_sem_vizinha
+
+def particao_por_estacao(df,percent_datetime_partitioning_split):
+    df_treinamento = df \
+        .groupby('id_estacao', as_index=False).apply(lambda g: g.iloc[:max(1, int(len(g) * percent_datetime_partitioning_split))]) \
+        .reset_index(drop=True)
+    
+    df_validacao = df \
+        .groupby('id_estacao', as_index=False).apply(lambda g: g.iloc[max(1, int(len(g) * percent_datetime_partitioning_split)):]) \
+        .reset_index(drop=True)
+
+    return df_treinamento,df_validacao
+
+def feature_selection(df,feature_list,target):
+    X = df[feature_list]
+    y = df[target]
+    return X,y
